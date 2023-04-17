b9579093e48baae01950050493c1b57cfbd67437
DinG <96579381+Joshua-DinG@users.noreply.github.com>
2023-04-18 02:01:54 +0800

    Update start.sh

--- qq/start.sh
+++ qq/start.sh
@@ -4,10 +4,6 @@ export DISPLAY=${DISPLAY:-:0}
 XVFB_WHD=${XVFB_WHD:-1280x720x24}
 XVFB_SCREEN=${XVFB_SCREEN:-0}
 
-cd /app
-
-# Installing fonts
-cp fonts/* /usr/share/fonts/
 
 Xvfb $DISPLAY -ac -screen $XVFB_SCREEN $XVFB_WHD -nolisten tcp &
 
