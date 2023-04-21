4ce9ec9841decf61c919706681fcd308a3fd986a
DinG <96579381+Joshua-DinG@users.noreply.github.com>
2023-04-21 20:25:06 +0800

    Update chatgpt-qq-bot.sh

--- chatgpt-qq-bot.sh
+++ chatgpt-qq-bot.sh
@@ -53,6 +53,7 @@ then
     echo "正在安装 docker-compose..."
     sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
+    sudo usermod -aG docker $USER && newgrp docker
 
 
     
